5b3a68f94162eb17606af83261c49280caa21908
Michael Chilaka <obichilaka@gmail.com>
2022-05-11 16:45:30 +0100

    updates on SinglePost.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import sanityClient from "../client";
 
 const About = () => {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    sanityClient.fetch(`*[_type == "author"]{
+      name,
+      bio,
+      "authorImage": image.asset->url
+    }`)
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  }, []); 
+
   return (
     <div>About</div>
   )
