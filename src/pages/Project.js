ee06c694889a13ff133773263c97a367301fe411
Michael Chilaka <obichilaka@gmail.com>
2022-05-11 07:09:01 +0100

    set-up & update of Project.js both on sanity and react

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import sanityClient from "../client";
 
 const Project = () => {
+  const [projectData, setProjectData] = useState(null);
+
+  useEffect(() => {
+    sanityClient.fetch(`*[_type == "project"]{
+      title,
+      date,
+      place,
+      description,
+      projectType,
+      link,
+      tags
+    }`)
+      .then((data) => setProjectData(data))
+      .catch(console.error);
+  }, []);
   return (
-    <div>Project</div>
+    <div></div>
   )
 }
 
