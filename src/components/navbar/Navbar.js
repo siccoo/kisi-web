3d7c437b705ac40783d439f75423375365fff6e6
Michael Chilaka <obichilaka@gmail.com>
2022-05-11 23:43:07 +0100

    modified Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom"
-import Kisi from "../../assets/img/Kisi.jpg";
 import "./Navbar.css"
 
 const Navbar = () => {
@@ -7,11 +6,11 @@ const Navbar = () => {
         <header>
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
-                    <NavLink exact className="nav__link-header inflex-flex items-center py-6 px-3 mr-4 hover:text-white-400 text-2xl font-bold cursive tracking-widest" to="/" activeClassName="text-white">Kisi~Deals
+                    <NavLink exact className="nav__link-header inflex-flex items-center py-6 px-3 mr-4 hover:text-white-900 text-2xl font-bold cursive tracking-widest" to="/" activeClassName="text-white">Kisi~Deals
                     </NavLink>
-                    <NavLink to="/about" className="inline-flex items-center py-1 px-3 my-6 rounded text-white-100 hover:text-indigo-900" activeClassName="text-red-100">About</NavLink>
-                    <NavLink to="/project" className="inline-flex items-center py-1 px-3 my-6 rounded text-white-100 hover:text-indigo-900" activeClassName="text-red-100">Projects</NavLink>
-                    <NavLink to="/post" className="inline-flex items-center py-1 px-3 my-6 rounded text-white-100 hover:text-indigo-900" activeClassName="text-red-100">Blog</NavLink>
+                    <NavLink to="/about" className="inline-flex items-center py-1 px-3 my-6 rounded text-white-900 hover:text-indigo-900" activeClassName="text-red-100">About</NavLink>
+                    <NavLink to="/project" className="inline-flex items-center py-1 px-3 my-6 rounded text-white-900 hover:text-indigo-900" activeClassName="text-red-100">Projects</NavLink>
+                    <NavLink to="/post" className="inline-flex items-center py-1 px-3 my-6 rounded text-white-900 hover:text-indigo-900" activeClassName="text-red-100">Blog</NavLink>
                     {/* <NavLink to="/about">About</NavLink> */}
 
                 </nav>
