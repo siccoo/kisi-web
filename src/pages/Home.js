8f83d964e85d896cc637e1ac2f50889db78844a4
Michael Chilaka <obichilaka@gmail.com>
2022-05-11 12:38:30 +0100

    updates on Project.js & Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,10 @@ import KisiImg from "../assets/img/mikey.jpeg"
 
 const Home = () => {
   return (
-    <main>
-      <img src={KisiImg} alt="kisi-img" className="absolute object-cover w-full h-full"/>
+    <main className="bg-green-100 min-h-screen p-12">
+      {/* <img src={KisiImg} alt="kisi-img" className="absolute object-cover w-full h-full"/> */}
       <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hey... I'm Michael nicknamed <small>lakaJS</small></h1>
+        <h1 className="text-6xl font-bold flex justify-center cursive leading-none lg:leading-snug home-name">Hey... I'm Michael nicknamed</h1><small style={{fontSize: 30, paddingTop: 36, paddingLeft: 5, color: "#335eea", fontWeight: "550", fontFamily: "sans-serif"}}>lakaJS</small>
       </section>
     </main>
   )
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import sanityClient from "../client";
 
 const Project = () => {
@@ -18,7 +18,52 @@ const Project = () => {
       .catch(console.error);
   }, []);
   return (
-    <div></div>
+    <main className='bg-green-100 min-h-screen p-12'>
+      <section className='container mx-auto'>
+        <h1 className="text-5xl flex justify-center cursive">Kisi Projects</h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to Kisi projects section!</h2>
+        <section className="grid grid-cols-2 gap-8">
+          {
+            projectData && projectData.map((project, index) => (
+              <article className="relative rounded-lg shadow-xl bg-white p-16">
+                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-gray-700">
+                  <a
+                    href={project.link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >{project.title}</a>
+                </h3>
+                <div className="text-gray-500 text-xs space-x-4">
+                  <span>
+                    <strong className="font-bold">Finished on</strong>:{" "}
+                    {new Date(project.date).toLocaleDateString()}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Company</strong>:{" "}
+                    {project.place}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Type</strong>:{" "}
+                    {project.projectType}
+                  </span>
+                  <p className="my-6 text-lg text-gray-700 leading-relaxed">{project.description}</p>
+                  <a
+                    href={project.link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-500 font-bold hover:underline hover:text-gray-400 text-xl"
+                  >
+                    View the project{" "}
+                    <span role="img" aria-label="right pointer">👉</span>
+                  </a>
+                </div>
+              </article>
+            ))}
+        </section>
+      </section>
+    </main>
   )
 }
 
